91a7799bd46f0fa9253bfa9da59ee7199b448a44
LOPOS <wanandumboa@gmail.com>
2026-06-07 17:32:57 +0100

    fix: remover componente Scanner inexistente

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const LojaLayout = ({ children }) => {
 const AppRoutes = () => (
   <Routes>
     <Route path="/gestao/*" element={<Gestao />} />
-    <Route path="/scanner" element={<Scanner />} />
+   
     <Route
       path="/*"
       element={
